5f0509617c5d69b900faa8c7a3c65725ef7c4784
Krystal Raynes <rayneskrystal@gmail.com>
2018-06-13 09:24:08 -0700

    Update starter.py
    uhh i hope this doesnt change the main file if so im sorry

--- starter.py
+++ starter.py
@@ -9,3 +9,17 @@ with open('setosa_v_versicolor.csv', newline='') as csvfile:
 	
 	for row in csvreader:
 		slength = row[0]
+		swidth = row[1]
+		plength = row[2]
+		pwidth = row[3]
+		species = row[4]
+		#shouldn't this be column?
+	int flower = 0
+	while flower < 100:
+		if csvreader[flower] [plength] >= 3:
+			#this means that the speciese is Iris Virginica, speciecs 1
+		
+		
+		
+	
+		
